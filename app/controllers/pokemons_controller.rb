0b4c01a47fd33218e064189270b4023e3406d81a
ryanriddle@berkeley.edu <ryanriddle@berkeley.edu>
2015-10-26 17:02:39 -0700

    close

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,8 @@
+class PokemonsController < ApplicationController
+	def capture
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.trainer_id = current_trainer.id
+		@pokemon.save
+		redirect_to "/"
+	end
+end
